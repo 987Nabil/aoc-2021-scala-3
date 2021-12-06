ccff8e9a6a56de2d935356bb45e745c84f9b7065
Nabil Abdel-Hafeez <987Nabil@users.noreply.github.com>
2021-12-06 13:59:09 +0100

    Day 6 refactoring

--- src/main/scala/aoc/Day6.scala
+++ src/main/scala/aoc/Day6.scala
@@ -9,22 +9,20 @@ def day6(): Unit =
 
 def fishPopulationAfter(fish: Map[Int, Long], daysToObserve: Int): Long =
   (1 to daysToObserve)
-    .foldLeft(fish) { case (lastDayFish, _) =>
-      todayFishNotDeliveringOrNewBorn(lastDayFish)
-        + fishDeliveringNextWeek(lastDayFish)
-        + newBornFish(lastDayFish)
-    }
+    .foldLeft(fish)((lastDayFish, _) =>
+      notDeliveringOrNewBorn(lastDayFish) + justDelivered(lastDayFish) + newBorn(lastDayFish)
+    )
     .values
     .sum
 
 def fishCountByDueDate(fish: List[Int]): Map[Int, Long] =
   (0 to 8).map(i => i -> fish.count(_ == i).toLong).toMap
 
-def todayFishNotDeliveringOrNewBorn(lastDayFish: Map[Int, Long]): Map[Int, Long] =
-  (for i <- 1 to 8 yield i - 1 -> lastDayFish(i)).toMap
+def notDeliveringOrNewBorn(lastDayFish: Map[Int, Long]): Map[Int, Long] =
+  lastDayFish.tail.map { _ - 1 -> _ }.toMap
 
-def fishDeliveringNextWeek(lastDayFish: Map[Int, Long]): (Int, Long) =
-  6 -> (todayFishNotDeliveringOrNewBorn(lastDayFish)(6) + lastDayFish(0))
+def justDelivered(lastDayFish: Map[Int, Long]): (Int, Long) =
+  6 -> (notDeliveringOrNewBorn(lastDayFish)(6) + lastDayFish(0))
 
-def newBornFish(lastDayFish: Map[Int, Long]): (Int, Long) =
+def newBorn(lastDayFish: Map[Int, Long]): (Int, Long) =
   8 -> lastDayFish(0)
