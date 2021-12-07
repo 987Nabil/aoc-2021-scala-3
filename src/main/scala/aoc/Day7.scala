65ea08910a68c29e06c18f75a641035d1855abb6
Nabil Abdel-Hafeez <987Nabil@users.noreply.github.com>
2021-12-07 07:24:54 +0100

    Day 7

---
+++ src/main/scala/aoc/Day7.scala
@@ -0,0 +1,24 @@
+package aoc
+
+@main
+def day7(): Unit =
+  val positions = readResourceLines("day7.txt").head.split(',').map(_.toInt).toList
+  val costs     = cheapestCosts(positions.min to positions.max)
+
+  val (optPos, fuelUsed)   = costs(crabMarineFuelCosts1(positions))
+  val (optPos2, fuelUsed2) = costs(crabMarineFuelCosts2(positions))
+
+  println(s"part 1: $fuelUsed")
+  println(s"part 2: $fuelUsed2")
+
+def cheapestCosts(alignmentRange: Range)(costFn: Int => Int) =
+  alignmentRange.foldLeft((-1, Int.MaxValue)) { case (old @ (_, minCost), alignTo) =>
+    if minCost <= costFn(alignTo) then old
+    else (alignTo, costFn(alignTo))
+  }
+
+def crabMarineFuelCosts1(positions: List[Int])(alignTo: Int): Int =
+  positions.foldLeft(0)((usedFuel, nextPos) => usedFuel + (nextPos - alignTo).abs)
+
+def crabMarineFuelCosts2(positions: List[Int])(alignTo: Int) =
+  positions.map { nextPos => (1 to (nextPos - alignTo).abs).sum }.sum
