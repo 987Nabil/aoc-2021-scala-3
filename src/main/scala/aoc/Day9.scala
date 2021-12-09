679840a973ac308905ba5e33d9b70ab0bb03b8f2
Nabil Abdel-Hafeez <987Nabil@users.noreply.github.com>
2021-12-09 17:07:20 +0100

    Day 9 refactoring

--- src/main/scala/aoc/Day9.scala
+++ src/main/scala/aoc/Day9.scala
@@ -1,42 +1,40 @@
 package aoc
 
+import scala.annotation.tailrec
 import scala.util.chaining.*
 
 @main
 def day9(): Unit =
   val map = readResourceLines("day9.txt").map(_.grouped(1).toSeq.map(_.toInt))
 
-  def maybeAdjacent(x: Int, y: Int) =
-    map.lift(y).flatMap(_.lift(x)).map((x, y) -> _)
+  def maybeAdjacent(y: Int, x: Int) =
+    map.lift(y).flatMap(_.lift(x)).map(_ => (y, x))
 
-  def adjacentPoints(x: Int, y: Int) =
-    List(
-      maybeAdjacent(x, y - 1),
-      maybeAdjacent(x, y + 1),
-      maybeAdjacent(x - 1, y),
-      maybeAdjacent(x + 1, y)
-    ).flatten.toMap
+  def adjacentPoints(y: Int, x: Int): Set[(Int, Int)] =
+    Set(
+      maybeAdjacent(y - 1, x),
+      maybeAdjacent(y + 1, x),
+      maybeAdjacent(y, x - 1),
+      maybeAdjacent(y, x + 1)
+    ).flatten
 
   val lowPoints =
     for
       x <- map.head.indices
       y <- map.indices
-    yield
-      val point = map(y)(x)
-      if adjacentPoints(x, y).values.min <= point then None
-      else Some(adjacentPoints(x, y) + ((x, y) -> point) -> point)
-    end for
-  .flatten.toMap
+      adjPoints = adjacentPoints(y, x)
+      if adjPoints.map(map(_)(_)).min > map(y)(x)
+    yield y -> x
 
-  def basin(adjPoints: Map[(Int, Int), Int]): Map[(Int, Int), Int] =
-    adjPoints.foldLeft(adjPoints.filterNot(_._2 == 9)) { case (acc, ((x, y), value)) =>
-      acc ++ basin(adjacentPoints(x, y).filter(_._2 > value))
+  //@tailrec
+  def basin(adjPoints: Set[(Int, Int)]): Set[(Int, Int)] =
+    adjPoints.foldLeft(adjPoints.filterNot(map(_)(_) == 9)) { case (acc, (y, x)) =>
+      acc ++ basin(adjacentPoints(y, x).filter(map(_)(_) > map(y)(x)))
     }
 
   val basins =
-    for (adjPoints, lowPoint) <- lowPoints
-    yield basin(adjPoints)
+    for lowPoint <- lowPoints yield basin(Set(lowPoint))
 
-  println(s"part 1: ${lowPoints.values.sum + lowPoints.size}")
+  println(s"part 1: ${lowPoints.map(map(_)(_)).sum + lowPoints.size}")
 
   println(s"part 2: ${basins.map(_.size).toList.sorted.takeRight(3).product}")
