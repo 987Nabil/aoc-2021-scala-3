860d96173f4fac3aace4b5be83d1756dde8f3d75
Nabil Abdel-Hafeez <987Nabil@users.noreply.github.com>
2021-12-10 12:08:08 +0100

    Day 10 refactoring

--- src/main/scala/aoc/Day10.scala
+++ src/main/scala/aoc/Day10.scala
@@ -1,61 +1,37 @@
 package aoc
 
 import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.collection.mutable.Stack
 
 @main
 def day10(): Unit = day[10] {
-  val syntax = input
-
   val open  = Set('(', '<', '{', '[')
   val close = Set(')', '>', '}', ']')
 
-  val score =
-    Map(
-      ')' -> 3,
-      ']' -> 57,
-      '}' -> 1197,
-      '>' -> 25137
-    )
-
-  val score2 =
-    Map(
-      ')' -> 1,
-      ']' -> 2,
-      '}' -> 3,
-      '>' -> 4
-    )
+  val errorScore        = Map(')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137)
+  val autoCompleteScore = Map(')' -> 1, ']' -> 2, '}' -> 3, '>' -> 4)
 
   @tailrec
-  def firstInvalidBrace(stack: Stack[Char], line: String): (Option[Char], Option[Stack[Char]]) =
+  def invalidOrLeftOpenBraces(stack: String, line: String): Char | String =
     line.toList match
       case next :: _ =>
-        if open(next) then firstInvalidBrace(stack.push(next), line.tail)
-        else if (stack.head - next).abs <= 2 then firstInvalidBrace(stack.drop(1), line.tail)
-        else (Some(next), None)
-      case Nil       => (None, Some(stack))
+        if open(next) then invalidOrLeftOpenBraces(stack :+ next, line.tail)
+        else if (stack.head - next).abs <= 2 then invalidOrLeftOpenBraces(stack.drop(1), line.tail)
+        else next
+      case Nil       => stack
 
-  val invalidBracesScore = syntax
-    .flatMap { l =>
-      firstInvalidBrace(Stack.empty[Char].pushAll(l.takeWhile(open)), l.dropWhile(open))._1
+  val (errorLines, incompleteLines) =
+    input.map(l => invalidOrLeftOpenBraces(l.takeWhile(open), l.dropWhile(open))).pipe { x =>
+      (x.collect { case c: Char => c }, x.collect { case s: String => s })
     }
-    .map(score)
-    .sum
 
-  val bla = syntax
-    .flatMap { l =>
-      firstInvalidBrace(Stack.empty[Char].pushAll(l.takeWhile(open)), l.dropWhile(open))._2
-    }
-    .map {
-      _.toList
-        .flatMap(open => close.find(c => (open - c).abs <= 2))
-        .map(score2)
-        .foldLeft(0L)((score, next) => score * 5 + next)
-    }
-    .sorted
+  val invalidBracesScore = errorLines.map(errorScore).sum
 
-  println(s"bla = ${bla}")
+  val autoCompleteScores = incompleteLines.map {
+    _.flatMap(open => close.find(c => (open - c).abs <= 2))
+      .map(autoCompleteScore)
+      .foldLeft(0L)((score, next) => score * 5 + next)
+  }.sorted
+
   part[1](invalidBracesScore)
-  part[2](bla(bla.size / 2))
+  part[2](autoCompleteScores(autoCompleteScores.size / 2))
 }
