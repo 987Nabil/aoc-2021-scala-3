a496c3dba6348e9609dba9d9e87f8afee881ba08
Nabil Abdel-Hafeez <987Nabil@users.noreply.github.com>
2021-12-12 07:36:11 +0100

    Day 12 refactoring

--- src/main/scala/aoc/Day12.scala
+++ src/main/scala/aoc/Day12.scala
@@ -1,5 +1,7 @@
 package aoc
 
+import aoc.Day12Types.*
+
 import scala.annotation.tailrec
 import scala.collection.MapView
 
@@ -7,31 +9,51 @@ import scala.collection.MapView
 def day12(): Unit = day[12] {
 
   val connections =
-    input.flatMap { case s"$x-$y" => List(x -> y, y -> x) }
-      .filterNot(_._2 == "start").groupMap(_._1)(_._2) - "end"
+    input
+      .flatMap { case s"$x-$y" => List(Cave(x) -> Cave(y), Cave(y) -> Cave(x)) }
+      .filterNot(_.connectsToStart)
+      .filterNot(_.connectsFromEnd)
+      .groupMap(_._1)(_._2)
+
+  def deadEnd(path: Path) =
+    path.smallCaves.caveVisits.values.exists(_ > 1)
+
+  def deadEnd2(path: Path) =
+    path.smallCaves.caveVisits.values.count(_ == 2) > 1
+      || path.smallCaves.caveVisits.values.exists(_ > 2)
 
   @tailrec
-  def paths(visited: List[List[String]]): List[List[String]] =
+  def paths(visited: List[Path], deadEnd: Path => Boolean): List[Path] =
     val newPaths = visited.flatMap {
-      case path if path.last == "end" => List(path)
-      case path if path.groupMapReduce(identity)(_ => 1)(_ + _).filterKeys(_.forall(_.isLower)).values.exists(_ > 1) => List()
-      case path                       => connections(path.last).map(x => path :+ x)
+      case path if path.endReached => List(path)
+      case path                    => connections(path.currentCave).map(path.goTo)
     }
-    if newPaths == visited then visited else paths(newPaths)
+    if newPaths == visited then visited else paths(newPaths.filterNot(deadEnd), deadEnd)
 
-  def filterPaths2(path: List[String]) =
-    val smallCavesCounts = path.groupMapReduce(identity)(_ => 1)(_ + _).filterKeys(_.forall(_.isLower)).values
-    smallCavesCounts.count(_ == 2) > 1 || smallCavesCounts.exists(_ > 2)
-
-  @tailrec
-  def paths2(visited: List[List[String]]): List[List[String]] =
-    val newPaths = visited.flatMap {
-      case path if path.last == "end" => List(path)
-      case path if filterPaths2(path) => List()
-      case path                       => connections(path.last).map(x => path :+ x)
-    }
-    if newPaths == visited then visited else paths2(newPaths)
-
-  part[1](paths(List(List("start"))).size)
-  part[2](paths2(List(List("start"))).size)
+  part[1](paths(List(Path.Start), deadEnd).size)
+  part[2](paths(List(Path.Start), deadEnd2).size)
 }
+
+object Day12Types:
+  opaque type Cave = String
+  opaque type Path = List[Cave]
+
+  object Cave:
+    def apply(cave: String): Cave = cave
+    val End: Cave                 = "end"
+    val Start: Cave               = "start"
+
+  object Path:
+    val Start: Path = List(Cave.Start)
+    val End: Path   = List(Cave.End)
+
+  extension (path: Path)
+    def smallCaves: Path           = path.filter(_.forall(_.isLower))
+    def caveVisits: Map[Cave, Int] = path.groupMapReduce(identity)(_ => 1)(_ + _)
+    def goTo(cave: Cave): Path     = path :+ cave
+    def currentCave: Cave          = path.last
+    def endReached: Boolean        = path.currentCave == Cave.End
+
+  extension (connection: (Cave, Cave))
+    def connectsToStart: Boolean = connection._2 == Cave.Start
+    def connectsFromEnd: Boolean = connection._1 == Cave.End
